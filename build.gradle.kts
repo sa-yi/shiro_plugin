ee26e6e18a19b88606795fb04a52e7d09b220bf4
飒翊 <2559806259@qq.com>
2025-03-21 20:09:25 +0800

    package dependencies into jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,41 +7,65 @@ group = "com.sayi.demo_plugin"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven { url =uri("https://maven.aliyun.com/repository/public/") }
-
+    maven { url = uri("https://maven.aliyun.com/repository/public/") }
     mavenCentral()
-
-    /*flatDir{
-        dirs("libs")
-    }*/
 }
 
 dependencies {
 
     //implementation(files("libs/shiro-2.3.6.jar"))
     api("org.springframework.boot:spring-boot-starter-websocket:3.4.0")
-    implementation("com.mikuac:shiro:2.3.6")
+    api("org.springframework.boot:spring-boot-starter:3.4.0")
+    compileOnly("com.mikuac:shiro:2.3.6")
 
 
-    implementation("org.springframework.boot:spring-boot-starter:3.4.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
+
+
+    implementation("cn.bigmodel.openapi:oapi-java-sdk:release-V4-2.3.1") {
+        exclude(group = "ch.qos.logback", module = "logback-classic")
+        exclude(group = "org.slf4j", module = "slf4j-api")
+        exclude(group = "org.apache.logging.log4j")
+    }
+    compileOnly("ch.qos.logback:logback-classic")
+    compileOnly("org.slf4j:slf4j-api")
+
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.jar {
+
+tasks.withType<Jar> {
     duplicatesStrategy = DuplicatesStrategy.INCLUDE
 
     from(sourceSets.main.get().output)
 
     manifest {
         attributes(
-            "Implementation-Title" to "DemoPlugin",
-            "Built-By" to System.getProperty("user.name")
+            mapOf(
+                "Implementation-Title" to "DemoPlugin",
+                "Built-By" to System.getProperty("user.name")
+            )
         )
-    }
+        // 生成并添加依赖清单
+        val dependenciesString = configurations
+            .getByName("runtimeClasspath") // 使用运行时配置获取实际解析的依赖
+            .resolvedConfiguration
+            .resolvedArtifacts
+            .map {
+                "${it.moduleVersion.id.group}:${it.moduleVersion.id.name}:${it.moduleVersion.id.version}"
+            }
+            .distinct()
+            .filterNot {//一些库应当由Shiro主程序加载
+                it.startsWith("org.springframework") || // 过滤Spring Boot
+                        it.startsWith("com.mikuac:shiro") // 过滤shiro核心库
+            }
+            .joinToString(", ")
 
-    // 修改include为包含全部编译结果
-    includeEmptyDirs = false
+        attributes(mapOf("Dependencies" to dependenciesString))
+    }
 }
--- src/main/java/com/sayi/demo_plugin/DemoPlugin.java
+++ src/main/java/com/sayi/demo_plugin/DemoPlugin.java
@@ -4,18 +4,93 @@ package com.sayi.demo_plugin;
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.core.BotPlugin;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zhipu.oapi.ClientV4;
+import com.zhipu.oapi.Constants;
+import com.zhipu.oapi.service.v4.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 
 @Component
 public class DemoPlugin extends BotPlugin {
+    private static final Logger log = LoggerFactory.getLogger(DemoPlugin.class);
+
+    static {
+        try{
+            Class.forName("com.zhipu.oapi.ClientV4");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     @Override
     public int onGroupMessage(Bot bot, GroupMessageEvent event) {
-        String msg = event.getMessage();
+        String msg = event.getRawMessage();
         if (msg.equals("ping")) {
             bot.sendGroupMsg(event.getGroupId(), "pong", false);
             return MESSAGE_BLOCK;
         }
+        if(msg.startsWith("/聊天")){
+            chat(bot,event);
+        }
         return MESSAGE_IGNORE;
+    }
+
+
+    private static final String API_KEY = System.getenv("GLM_API_TOKEN");
+
+    private static List<ChatTool> tools;
+    private static List<ChatMessage> completionPayload;
+    private static ObjectMapper MAPPER = new ObjectMapper();
+    private static ModelApiResponse call_api(ChatMessage input) {
+        completionPayload.add(new ChatMessage(ChatMessageRole.SYSTEM.value(), "你是一个人工智能助手"));
+        completionPayload.add(input);
+
+
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model("glm-4v")
+                .stream(Boolean.FALSE)
+                .invokeMethod(Constants.invokeMethod)
+                .messages(completionPayload)
+                .build();
+
+
+        ModelApiResponse response = client.invokeModelApi(chatCompletionRequest);
+
+        return response;
+    }
+    static {
+        completionPayload = new ArrayList<>();
+    }
+
+    private static final ClientV4 client = new ClientV4.Builder(API_KEY)
+            .enableTokenCache()
+            .networkConfig(30, 10, 10, 10, TimeUnit.SECONDS)
+            .connectionPool(new okhttp3.ConnectionPool(8, 1, TimeUnit.SECONDS))
+            .build();
+
+    private void chat(Bot bot, GroupMessageEvent event){
+        String content=event.getRawMessage().substring(3);
+        log.info(content);
+        ChatMessage message = new ChatMessage(ChatMessageRole.USER.value(), content);
+        ModelApiResponse response = call_api(message);
+        if(response.isSuccess()) {
+            ModelData modelData = response.getData();
+            Choice choice = modelData.getChoices().get(0);
+            if(choice.getFinishReason().equals("stop")) {
+                String msg= (String) choice.getMessage().getContent();
+                bot.sendGroupMsg(event.getGroupId(), msg, false);
+            }
+        }else {
+            bot.sendGroupMsg(event.getGroupId(),response.toString(),false);
+        }
     }
 }

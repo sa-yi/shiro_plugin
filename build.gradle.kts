2bb25a24505d37170d3a280d8c5ed11f7c1eb6a5
飒翊 <2559806259@qq.com>
2026-07-12 13:27:50 +0800

    更新了示例代码

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     `java-library`
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
 }
-
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 group = "com.sayi.demo_plugin2"
 version = "1.0-SNAPSHOT"
 
@@ -13,7 +19,6 @@ repositories {
 dependencies {
     implementation("com.mikuac:shiro:2.5.4")
 
-    implementation("org.springframework.boot:spring-boot-starter-websocket:3.2.0")
     implementation("org.springframework.ai:spring-ai-starter-model-deepseek:1.1.4")
 
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
@@ -23,17 +28,9 @@ dependencies {
 
     implementation("cn.hutool:hutool-all:5.8.47")
 
-    implementation("cn.bigmodel.openapi:oapi-java-sdk:release-V4-2.3.1") {
-        exclude(group = "ch.qos.logback", module = "logback-classic")
-        exclude(group = "org.slf4j", module = "slf4j-api")
-        exclude(group = "org.apache.logging.log4j")
-    }
-    compileOnly("ch.qos.logback:logback-classic")
     compileOnly("org.slf4j:slf4j-api")
+    compileOnly("ch.qos.logback:logback-classic")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
 }
 
 tasks.test {
@@ -63,10 +60,6 @@ tasks.withType<Jar> {
                 "${it.group}:${it.name}:${it.version}"
             }
             .distinct()
-            .filterNot {
-                it.startsWith("org.springframework") ||
-                        it.startsWith("com.mikuac:shiro")
-            }
             .joinToString(", ")
 
         attributes(mapOf("Dependencies" to dependenciesString))
